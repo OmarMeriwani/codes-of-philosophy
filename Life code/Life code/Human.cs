8a556d33d835fab7963d5746604cce373b5fca35
Omar Meriwani <omar.sirwan@gmail.com>
2017-07-16 16:37:37 +0300

    --

---
+++ Life code/Life code/Human.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Life_code
+{
+    class Human
+    {
+        public Human(Human father, Human mother)
+        {
+
+        }
+        public Int64 Money { get; set; }
+        public List<Gene> genes { get; set; }
+        public Country country { get; set; }
+        public string relegion { get; set; }
+        public double Pain { get; set; }
+        public double Love { get; set; }
+        public int Knowledge { get; set; }
+    }
+}
